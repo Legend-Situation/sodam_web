0f9db60fc009b50ccceec475e31d99aebc36a8e2
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-02-16 06:02:54 +0900

    feat: 채팅 페이지 퍼블리싱

---
+++ src/pages/Chat/index.tsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from "react";
+import * as S from "./style";
+import Send from "../../assets/send.svg";
+import Arrow from "../../assets/arrow.svg";
+
+interface Comment {
+  author: string;
+  text: string;
+}
+
+const Chat = () => {
+  const [inputValue, setInputValue] = useState<string>("");
+  const [comments, setComments] = useState<Comment[]>([]);
+
+  useEffect(() => {
+    const storedComments = localStorage.getItem("chatComments");
+    if (storedComments) {
+      setComments(JSON.parse(storedComments));
+    }
+  }, []);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSendClick = () => {
+    if (inputValue.trim() !== "") {
+      const newComment = { author: "띠연", text: inputValue };
+      const updatedComments = [...comments, newComment];
+      setComments(updatedComments);
+      setInputValue("");
+
+      localStorage.setItem("chatComments", JSON.stringify(updatedComments));
+    }
+  };
+
+  return (
+    <S.Layout>
+      <S.Header>
+        <S.Arrow src={Arrow} />
+        <span>질문 #12</span>
+      </S.Header>
+      <S.Question>서로를 볼 때 생각나는 동물은 무엇인가요?</S.Question>
+      <S.OtherChatContainer>
+        <span>김사장</span>
+        <S.Chat>내가 개구리? 너무한거 아니냐.</S.Chat>
+      </S.OtherChatContainer>
+      <S.OtherChatContainer>
+        <span>엄마다</span>
+        <S.Chat>ㅋ</S.Chat>
+      </S.OtherChatContainer>
+      <S.OtherChatContainer>
+        <span>엄마다</span>
+        <S.Chat>인정해요 그냥.</S.Chat>
+      </S.OtherChatContainer>
+
+      {comments.map((comment, index) =>
+        comment.author === "나" ? (
+          <S.MyChatContainer key={index}>
+            <span>나</span>
+            <S.MyChat>{comment.text}</S.MyChat>
+          </S.MyChatContainer>
+        ) : (
+          <S.OtherChatContainer key={index}>
+            <span>{comment.author}</span>
+            <S.Chat>{comment.text}</S.Chat>
+          </S.OtherChatContainer>
+        ),
+      )}
+      <S.InputContainer>
+        <S.Input
+          placeholder="댓글달기..."
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <img
+          src={Send}
+          width={24}
+          height={24}
+          onClick={handleSendClick}
+          style={{ cursor: "pointer" }}
+          alt="Send"
+        />
+      </S.InputContainer>
+    </S.Layout>
+  );
+};
+
+export default Chat;
---
+++ src/pages/Chat/style.ts
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+import Background from "../../assets/background.png";
+
+export const Layout = styled.main`
+  background-image: url(${Background});
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  font-family: "SeoulHangangM";
+`;
+export const Header = styled.header`
+  padding: 57px 28px 30px 28px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+`;
+export const Arrow = styled.img`
+  position: absolute;
+  left: 20px;
+`;
+export const Question = styled.div`
+  text-align: center;
+  padding-bottom: 50px;
+`;
+export const OtherChatContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px 27px;
+  font-size: 14px;
+`;
+export const Chat = styled.span`
+  margin-top: 3px;
+  background-color: #ececec;
+  padding: 2px 10px;
+  border-radius: 8px;
+  width: fit-content;
+  font-size: 16px;
+`;
+export const InputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 7%;
+  background-color: white;
+`;
+export const Input = styled.input`
+  width: 90%;
+  height: 90%;
+  border: none;
+  padding: 0 20px;
+  font-size: 16px;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const MyChatContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  padding: 10px 27px;
+  font-size: 14px;
+`;
+
+export const MyChat = styled.span`
+  margin-top: 3px;
+  background-color: #ececec;
+  padding: 2px 10px;
+  border-radius: 8px;
+  width: fit-content;
+  font-size: 16px;
+`;
