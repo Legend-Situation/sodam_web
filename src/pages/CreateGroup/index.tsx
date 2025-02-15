686f167547ce47222a24cf1dc5f2fb1226f87ef8
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-02-16 01:00:32 +0900

    feat: 그룹 생성하기 페이지 퍼블리싱

---
+++ src/pages/CreateGroup/index.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import * as S from "./style";
+
+const CreateGroup = () => {
+  const [inputGroup, setInputGroup] = useState("");
+  const [inputDate, setInputDate] = useState("");
+
+  const handleInputGroup = (e: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
+    setInputGroup(e.target.value);
+  };
+  const handleInputDate = (e: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
+    setInputDate(e.target.value);
+  };
+  return (
+    <S.Layout>
+      <S.Header>
+        <span>그룹 생성하기</span>
+      </S.Header>
+      <S.MainContainer>
+        <span>그룹 이름</span>
+        <S.GroupNameInput
+          placeholder="그룹 이름을 입력해주세요. (최대 10자)"
+          value={inputGroup}
+          onChange={handleInputGroup}
+        />
+        <span>관계 시작 날짜</span>
+        <S.StartDateInput
+          placeholder="2000/01/01"
+          value={inputDate}
+          onChange={handleInputDate}
+        />
+      </S.MainContainer>
+      <S.BtnContainer>
+        <S.CheckBtn isActive={inputGroup.length > 0 && inputDate.length > 0}>
+        확인
+      </S.CheckBtn>
+      </S.BtnContainer>
+      
+    </S.Layout>
+  );
+};
+
+export default CreateGroup;
---
+++ src/pages/CreateGroup/style.ts
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+import Background from "../../assets/background.png";
+
+export const Layout = styled.main`
+  background-image: url(${Background});
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  font-family: "SeoulHangangM";
+  position: relative;
+  font-size: 16px;
+`;
+
+export const Header = styled.header`
+  padding: 62px 28px 10px 10px;
+  text-align: center;
+`;
+
+export const MainContainer = styled.div`
+  padding: 10px 28px 0 28px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const GroupNameInput = styled.input`
+  width: 90%;
+  border-radius: 8px;
+  border: 1px solid #84c3ee;
+  padding: 12px 16px;
+  outline: none;
+  font-size: 16px;
+  margin: 8px 0 20px 0;
+`;
+
+export const StartDateInput = styled.input`
+  width: 90%;
+  border-radius: 8px;
+  border: 1px solid #84c3ee;
+  padding: 12px 16px;
+  outline: none;
+  font-size: 16px;
+  margin: 8px 0 20px 0;
+`;
+
+export const BtnContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const CheckBtn = styled.button<{ isActive: boolean }>`
+  position: absolute;
+  width: 80%;
+  padding: 12px 0;
+  bottom: 64px;
+  background-color: ${(props) => (props.isActive ? "#84C3EE" : "#dfdfdf")};
+  border-radius: 8px;
+  color: ${(props) => (props.isActive ? "#ffffff" : "#8a8a8a")};
+  border: none;
+  cursor: ${(props) => (props.isActive ? "pointer" : "default")};
+`;
