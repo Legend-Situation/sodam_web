c4abfb4fea0dc04051c88268bc83a4c0e0984855
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-02-15 21:27:40 +0900

    chore: 프로젝트 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import WriteName from "./pages/Write";
-import Main from "./pages/Main";
-import ConntextFamily from "./pages/ConnectFamily";
-import QuestionDetail from "./pages/QuestionDetail";
-import AnswerModal from "./pages/AnswerModal";
-import AnswerPage from "./pages/AnswerPage";
-import ChatPage from "./pages/ChatPage";
-import List from "./pages/List";
-import Login from "./pages/Login";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ConntextFamily />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/write-name" element={<WriteName type="name" />} />
-        <Route path="/write-petname" element={<WriteName type="petName" />} />
-        <Route path="/main" element={<Main />} />
-        <Route path="/question-detail" element={<QuestionDetail />} />
-        <Route path="/answer" element={<AnswerModal />} />
-        <Route path="/result" element={<AnswerPage />} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="/list" element={<List />} />
+        <Route path="/" element={<Login />} />
       </Routes>
     </Router>
   );
