b64048a55d7c59b8fba8aecac59839ce3b7e12d5
추성우 <126847458+chooseongwoo@users.noreply.github.com>
2025-07-02 10:47:33 +0900

    Attach access token header

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,10 +1,21 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
+const API_BASE_URL = (import.meta.env.VITE_API_BASE_URL as string) || '';
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
   withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use((config: any) => {
+  const accessToken = localStorage.getItem('accessToken');
+  if (accessToken) {
+    config.headers = {
+      ...config.headers,
+      accessToken,
+    } as any;
+  }
+  return config;
 });
 
 export default axiosInstance;
